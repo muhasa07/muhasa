654b4334acd13cc9d0a0abb40b20d14a018b872e
fif07 <muhammadafifsaputra07@gmail.com>
2025-02-10 15:04:50 +0700

    full project muhasa

---
+++ script.js
@@ -0,0 +1,96 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["IT Enthusiast", "Programmer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["IT Enthusiast", "Programmer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 30,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+
+    const $tabs = $('[data-target]');
+    const $tabContents = $('[data-content]');
+
+    $tabs.on('click', function() {
+        const target = $(this).data('target'); // Ambil nilai data-target dari tab yang diklik
+        const $targetContent = $(target); // Seleksi elemen konten yang sesuai
+
+        // Sembunyikan semua konten
+        $tabContents.removeClass('project-active');
+
+
+        // Tampilkan konten yang sesuai
+        $targetContent.addClass('project-active');
+
+
+        // Nonaktifkan semua tab
+        $tabs.removeClass('project-active');
+
+
+        // Aktifkan tab yang diklik
+        $(this).addClass('project-active'); 
+    });
+});
